9f72b62bbecd7169d734b19686bc670530c95716
John Rumide <johnrumide6@gmail.com>
2022-05-22 21:16:45 -0700

    fix print_addr function

--- print_address.c
+++ print_address.c
@@ -10,13 +10,14 @@ int print_addr(va_list arg)
 {
 	char *s;
 	unsigned long int n = va_arg(arg, unsigned long int);
-	int i;
+	int i = 0;
 
 	if (!n)
 		return (_puts("(nil)"));
-	s = convert(n, 16, 1)
-	    i += _puts("0x");
+	s = convert(n, 16, 1);
+	i += _puts("0x");
 	i += _puts(s);
 
 	return (i);
 }
+
